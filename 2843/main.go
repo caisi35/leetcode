ace06413b3be8a29f28ebdf4b3901488c15ed91a
caisi35 <caisi1735@163.com>
2025-04-11 13:38:37 +0800

    统计对称整数的数目

---
+++ 2843/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println(countSymmetricIntegers (1, 2000))
+}
+
+func countSymmetricIntegers(low int, high int) int {
+	res := 0
+	for a := low; a <= high; a++ {
+		if a < 100 && a % 11 == 0 {
+			res++
+		} else if 1000 <= a && a < 10000 {
+			left := a / 1000 + (a%1000) / 100
+			right := (a%100)/10 + a % 10
+			if left == right {
+				res++
+			}
+		}
+	}
+	return res
+}
